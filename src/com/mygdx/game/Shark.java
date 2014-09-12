a48b7bde9cea60dd3e6507ddcb1778f29874b948
Zzeek <jwjordan@asu.edu>
2014-09-11 17:23:43 -0700

    update shark.java
    Yo proposing file change for shark

--- src/com/mygdx/game/Shark.java
+++ src/com/mygdx/game/Shark.java
@@ -1,25 +1,42 @@
-package com.mygdx.game;
+import java.util.Random;
 
 import com.badlogic.gdx.graphics.Texture;
 
+class Shark  {
+	private int xCoord; 
+	private int yCoord;
+	private int speed;
+	Random rand;
+	final OtterGame game;
+	// create an array of sharks and have them randomly spawn
+	// spawn coordinates
+	public Shark(final OtterGame gam) {
+		this.gam = gam;
+		
+		sharkSprite = new Texture("shark.png");
+		speed = 3;
+		rand = new Random();
+		xCoord = -100;
+		yCoord = rand.nextInt(480);
+	}
+	
+	
+	void movement() {
+		xCoord += speed;
+	}
+	
+	void animations() {}
+	
+	void hit() {}
+	
+	void positionReset() {}
 
-class Shark{
 
-	Texture shark;
-	
-	void spawn(){}
-	
-	void despawn(){}
-	
-	void display(){}
-	
-	void movement(){}
-	
-	void animations(){}
-	
-	void hit(){}
-	
-	void positionReset(){}
-	
-	
-}
+	public int getxCoord() {
+		return xCoord;
+	}
+
+
+	public int getyCoord() {
+		return yCoord;
+	}
